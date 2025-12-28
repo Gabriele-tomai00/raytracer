dc7ee8b86f5d1cb0bc6962213870abcd3d83500f
Gabriele <tomaigabriele@gmail.com>
2025-12-28 16:31:56 +0100

    first implementation of ppm.h

--- ppm.h
+++ ppm.h
@@ -3,3 +3,12 @@
     Student ID: IN2300006
     Degree Program: Computer Engineering
 */
+
+#ifndef _PPM_H
+#define _PPM_H
+
+#include "scene.h"
+
+int write_ppm(const char *path, const pixel *image, int width, int height);
+
+#endif
